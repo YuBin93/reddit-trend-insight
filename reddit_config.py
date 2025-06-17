5e419a29f9c4f2bda05519ce59307191bf5495b5
YuBin93 <urbeen12345@gmail.com>
2025-06-17 14:54:12 +0800

    Update reddit_config.py

--- reddit_config.py
+++ reddit_config.py
@@ -2,8 +2,8 @@ import os
 import praw
 
 def get_reddit_instance():
-    client_id = os.getenv("REDDIT_CLIENT_ID")
-    client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+    client_id = os.getenv("APTOH3quFr6I_jo4ntjDxg")
+    client_secret = os.getenv("4Rk73clDP9biN1Fi-wszS44RAhuLvA")
     user_agent = "reddit_trend_insight_app/0.1"
     reddit = praw.Reddit(client_id=client_id,
                          client_secret=client_secret,
